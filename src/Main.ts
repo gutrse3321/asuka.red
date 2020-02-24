effe43d02a37bc9c1a37587ce11794ec1ba25826
gutrse3321 <gutrse3321@live.com>
2020-02-24 06:12:46 -0800

    .feat: asuka element

--- src/Main.ts
+++ src/Main.ts
@@ -3,6 +3,7 @@ import {Render} from "./Render";
 import "./asset/css/index.styl"
 import {Background} from "./element/Background";
 import {Nerv} from "./element/Nerv";
+import {Asuka} from "./element/Asuka";
 
 class Main extends Render {
 
@@ -17,6 +18,7 @@ class Main extends Render {
         let mainEl = this.addMainElement();
         let background = new Background({className: "img__bg", parent: mainEl});
         let nerv = new Nerv({className: "img__nerv", parent: mainEl});
+        let asuka = new Asuka({className: "img__asuka", parent: mainEl});
     }
 }
 
--- src/element/Asuka.ts
+++ src/element/Asuka.ts
@@ -6,10 +6,15 @@
  * -----
  */
 import {Render} from "../Render";
+import AsukaWebPImage from "../asset/image/asuka.webp";
 
 export class Asuka extends Render {
 
   protected render(): void {
+    this.el = this.createElement("img");
+    this.addClass(this.el, <string> this.options.className);
+    this.appendChild(this.options.parent, this.el, false);
+    this.el.setAttribute("src", AsukaWebPImage);
   }
 
 }
