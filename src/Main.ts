1606badd411a8ced1e6ec31acfb2200b5a97ba96
Tomonori <gutrse3321@live.com>
2020-02-25 11:15:15 +0800

    .del

--- src/Main.ts
+++ src/Main.ts
@@ -3,7 +3,6 @@ import {Render} from "./Render";
 import "./asset/css/index.styl"
 import {Background} from "./element/Background";
 import {Nerv} from "./element/Nerv";
-import {Asuka} from "./element/Asuka";
 
 class Main extends Render {
 
@@ -18,7 +17,6 @@ class Main extends Render {
         let mainEl = this.addMainElement();
         let background = new Background({className: "img__bg", parent: mainEl});
         let nerv = new Nerv({className: "img__nerv", parent: mainEl});
-        let asuka = new Asuka({className: "img__asuka", parent: mainEl});
     }
 }
 
