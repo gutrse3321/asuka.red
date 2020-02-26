00744dd607b96a78997fd5b04a5279cc5df041fe
Tomonori <gutrse3321@live.com>
2020-02-26 18:51:59 +0800

    .feat:theme style

---
+++ src/views/Home.tsx
@@ -0,0 +1,12 @@
+/**
+ * @Author: Tomonori
+ * @Date: 2020/2/26 18:48
+ * @Title:
+ * @Desc: ↓ ↓ ↓ ↓ ↓
+ * -----
+ */
+import * as React from "react";
+
+export default class Home extends React.Component<any, any> {
+
+}
